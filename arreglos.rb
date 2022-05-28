4a7200728852b6d48ae558acbbf8efb98e6612c7
Lloyka Pérez <andreaperezharambour@gmail.com>
2022-05-28 22:12:53 +0000

    Ejercicios Arreglos

--- arreglos.rb
+++ arreglos.rb
@@ -1,7 +1,53 @@
-#Hola Que tal
-print "Aquí va mi trabajo de arreglos"
+#Ejercicios de arreglos
 
-x = [ 2, 3, 4, 5,]
+a = ["Elena", "Jose", "Violeta", "Mathias", "Leon"]
+b = [5, 6, 9, 3, 1, 2, 4, 7, 8, 10]
+c = ["Jose", 9]
 
-puts x
+# Retornos de los arreglos
+
+    # puts a[0] - retorna el indice 0 del arreglo a
+    # puts b[1] - retorna el indice 1 del arreglo b
+    # puts a+b - retorna la suma de los arreglos         a y b
+
+    # x = (a+b)-c - convinar un arreglo con + sumara sus indices y poniendo un - restará los indices
+
+    # puts x.to_s - convierte el arreglo x en un string
+
+    # puts b.class - indica la clase a la que pertenece el arreglo que se esta llamando.
+
+puts b.shuffle.join("-")
+
+puts a.to_s
+
+#Provando .at - imprime el valor del indice que se esta llamando 
+
+    #b1= b.at(2)
+    #puts b1
+
+#Provando .delete - elimina el indice que se llama
+
+    #a.delete("Jose")
+    #puts a
+
+#Provando .push - agrega el valor indicado al final del arreglo
+
+    #b.push(22)
+    #puts b 
+
+#Provando .pop - elimina el ultimo indice del arreglo
+
+    #b.pop()
+    #puts b
+
+#Provando .length - indica la cantidad de indices que tiene el arreglo que se llama
+
+    #puts "El largo de a es #{a.length}"
+
+#Provando .join
+
+#Provando .reverse
+
+#Provando .shuffle
+
 
